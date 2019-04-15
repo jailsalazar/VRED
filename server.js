9bfc89ce46298756379f24a7129a829310d16b2c
jailsalazar <salazarjaileen@gmail.com>
2019-04-14 22:11:26 -0700

    Added mongodb instance to heroku

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 // server.js
 
-var express = require('express');
-var path = require('path');
+var express = require("express");
+// var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+// var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 5000;
-app.listen(port);
+// var port = process.env.PORT || 5000;
+// app.listen(port);
 
-console.log('Server started ' + port);
+// console.log('Server started ' + port);
+
+var CONTACTS_COLLECTION = "contacts";
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+
