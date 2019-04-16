223d70821734deb1d1ce7f87acc1bf085298f146
jailsalazar <salazarjaileen@gmail.com>
2019-04-16 16:06:25 -0700

    Added user endpoints.

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 // server.js
 
 var express = require("express");
-// var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
-// var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-
-// console.log('Server started ' + port);
-
-var CONTACTS_COLLECTION = "contacts";
+var USER_COLLECTION = "users";
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
@@ -38,4 +31,42 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
   });
 });
 
+//API ROUTES
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/api/users"
+ *    GET: finds all users
+ *    POST: creates a new user
+ */
+app.get("/api/users", function(req, res) {
+  db.collection(USER_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get users.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/users", function(req, res) {
+  var newUser = req.body;
+  newUser.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  } else {
+    db.collection(USER_COLLECTION).insertOne(newUser, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new user.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
+});
 
