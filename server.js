b36bfabf790c2a044704a3ae7f4770b7ef67521c
jailsalazar <salazarjaileen@gmail.com>
2019-04-18 02:53:46 -0700

    * route

--- server.js
+++ server.js
@@ -30,7 +30,16 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://vred:vred19@ds
     var port = server.address().port;
     console.log("App now running on port", port);
   });
+  if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+  }
+  
+  app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 });
+
 
 //API ROUTES
 // Generic error handler used by all endpoints.
